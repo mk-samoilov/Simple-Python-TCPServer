e58509a970aa95c9a507a188710cc42b92093368
mk-samoilov <maksim.samoilov.0202@yandex.ru>
2024-10-28 11:30:30 +0300

    Update config.py
    Adding secret key settings (lol it is not used anywhere yet but will be soon)

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 HOST = "0.0.0.0"
 PORT = 6070
 MAX_CLIENTS = 5
-PLUGIN_DIR = "plugins"
+PLUGINS_DIR = "plugins"
 LOADING_PLUGINS = True
+SECRET_KEY_HASH = "f427e5841fd6b0512c4536a00e590b3cd41676b0c44a88f555da903b835bbab8"
+
+# (sha256 secret key hash)
+# simple sicret key: USTER
